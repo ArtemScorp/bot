f31b8a3507336129a8b86b827fdff012118e44a5
artem.scorp <artem.scorp@ya.ru>
2026-03-22 19:43:22 +0800

    commit

--- bot_mem.py
+++ bot_mem.py
@@ -1,9 +1,20 @@
-import telebot
+import os, random
+from telebot import TeleBot
 
+folder = 'images'
 API_TOKEN = '8600769859:AAE6UQ_AvUHvPBqx3zwfhPzg3RrD81Pegs8'
 
-telebot.teleBot = 'API_TOKEN'
+bot = TeleBot(API_TOKEN) 
+ss = os.listdir(f'{folder}')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    sa = random.choice(ss)
+    with open(f'{folder}/{sa}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)  
+    #bot.reply_to(message, sa)
+
+ #    bot.reply_to(message, f"Таймер подёт прямо сейчас!!!")
+    
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
